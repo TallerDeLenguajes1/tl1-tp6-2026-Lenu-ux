ff8584c741557731563728bb582840c5c425367e
Lenis <lenis99lg@gmail.com>
2026-05-19 16:33:53 -0300

    ejercicio2 CalculadoraV1

--- Program.cs
+++ Program.cs
@@ -39,3 +39,73 @@ if (int.TryParse(c, out n))
 {
     Console.WriteLine("Entrada no valida...Ingrese un numero");
 }
+//ejercicio 2
+string ? opcion;
+           
+    do
+        {
+            Console.WriteLine("Seleccione la operacion a realizar:");
+            Console.WriteLine("1.Suma");
+            Console.WriteLine("2.Resta");
+            Console.WriteLine("3.Multiplicacion");
+            Console.WriteLine("4.Division");
+            Console.WriteLine("5.Salir");
+            int resultado = 0;
+            Console.WriteLine("Ingrese su eleccion:");
+            opcion=Console.ReadLine();
+            if (int.TryParse(opcion, out n))
+            {
+                if (n != 5)
+                {
+                     Console.WriteLine("Ingrese el primer numero:");
+            string ?N1;
+            N1=Console.ReadLine();
+            Console.WriteLine("Ingrese el segundo numero:");
+            string ?N2;
+            N2=Console.ReadLine();
+            
+
+            if (int.TryParse(N1,out a) && int.TryParse(N2,out b))
+            {
+                switch (n)
+            {
+                case 1:
+                        resultado=a+b;
+            Console.WriteLine("Resultado :"+ resultado);
+                        break;
+                case 2:
+                        resultado=a-b;
+            Console.WriteLine("Resultado :"+ resultado);
+
+                        break;
+                case 3:
+                        resultado=a*b;
+            Console.WriteLine("Resultado :"+ resultado);
+
+                        break;
+                case 4:
+                        resultado=a/b;
+            Console.WriteLine("Resultado :"+ resultado);
+
+                        break;
+                default:
+                break;
+
+
+            }
+                } else
+                {
+                    Console.WriteLine("Entrada no valida...Ingrese un numero");
+                }
+            }
+           
+            }else
+            {
+                Console.WriteLine("Entrada no valida...Ingrese un numero");
+    
+            }
+
+        } while (n !=5);
+
+//ejercicio 3
+        
